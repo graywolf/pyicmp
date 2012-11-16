2396a196cdd8dfd2f1705e8d4eef7d25353ba147
Tomas Paladin Volf <volf.tomas@gmail.com>
2012-11-16 16:40:56 +0100

    Added mdev reporting ability

--- ping.py
+++ ping.py
@@ -8,53 +8,78 @@ import socket
 
 Because it is supposed to be used in data collection etc, there is no
 reason to fool around with output etc. It just collects data, how you
-use them is up to you"""
+use them is up to you.
+
+If you run directly this script, it just prints out everything it knows."""
 class Ping:
 	
 	ip = None
 	on = False
 	repeat = 10
 	
-	avg_time = 0
+	times = []
+	avg_time = None
 	max_time = None
 	min_time = None
+	mdev = None
 	packet_loss = 0
 	responses = []
 	
+	"""Init class
+	
+	args:
+		ip	ip adress of target
+		run	True for auto run, if False you must call do_ping() to collect data
+	
+	You will use run = False mostly if you want to change repeat value (how
+	many times repeat the measurement)."""
 	def __init__(self, ip, run = True):
 		self.ip = ip
 		if run:
 			self.do_ping()
 	
+	"""Do the ping. self.ip must be setted."""
 	def do_ping(self):
+		#do the job repeat-times
 		for i in range(0, self.repeat):
-			try:
-				#consider moving time measuring into handle_packet for
-				#removing measuring func calling cost
-				reply, delta = handler.handle_packet(self.ip, messages.EchoRequest())
-				if type(reply) == messages.EchoReply:
-					self.on = True
-					microseconds = delta.seconds * 1000000 + delta.microseconds
-					self.avg_time += microseconds
-					
-					try:
-						if self.max_time < microseconds:
-							self.max_time = microseconds
-					except TypeError as e:
+			#reply type & timing
+			reply, delta = handler.handle_packet(self.ip, messages.EchoRequest())
+			#was Echo request a success?
+			if type(reply) == messages.EchoReply:
+				#target is out there
+				self.on = True
+				#convert delta to microseconds and append to list with times
+				microseconds = delta.seconds * 1000000 + delta.microseconds
+				self.times.append(microseconds)
+				
+				#set/update min&max times
+				try:
+					if self.max_time < microseconds:
 						self.max_time = microseconds
-					
-					try:
-						if self.min_time > microseconds:
-							self.min_time = microseconds
-					except TypeError as e:
+				except TypeError:
+					self.max_time = microseconds
+				
+				try:
+					if self.min_time > microseconds:
 						self.min_time = microseconds
-				else:
-					self.packet_loss += 1
-				self.responses.append(reply)
-			except Exception as e:
-				raise
+				except TypeError:
+					self.min_time = microseconds
+			#could not get through, increase packet loss
+			else:
+				self.packet_loss += 1
+			#log response
+			self.responses.append(reply)
 		
-		self.avg_time /= self.repeat - self.packet_loss
+		#number of successes
+		ok = self.repeat - self.packet_loss
+		#if at least one packet was ok
+		if ok != 0:
+			#calculate mean deviation
+			mean = sum([float(x) for x in self.times]) / len(self.times)
+			self.mdev = sum([abs(x - mean) for x in self.times]) / len(self.times)
+			#calculate average time
+			self.avg_time = sum(self.times) / len(self.times)
+		#convert #packet loss to [0;1] percentage value
 		self.packet_loss /= self.repeat
 
 if __name__ == '__main__':
@@ -67,5 +92,10 @@ if __name__ == '__main__':
 		print ('Average time:', p.avg_time, 'µs')
 		print ('Maximum time:', p.max_time, 'µs')
 		print ('Minimum time:', p.min_time, 'µs')
+		print ('MDev:', p.mdev, 'µs')
 		print ('Packet loss:', p.packet_loss)
+		
+		print ('\nTimes:', p.times, 'µs')
+		
+		print ('\nResponses:', [x.__class__.__name__ for x in p.responses])
 
